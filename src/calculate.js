f1ade36b98a38b1213417b68f9067b76abe37f6d
Vasilyev-Anton <vasilyev1812@gmail.com>
2023-08-19 21:24:13 +0300

    init

---
+++ src/calculate.js
@@ -0,0 +1,10 @@
+export default function calculateTotal(purchases, applyDiscount) {
+  const sum = purchases.reduce((total, item) => {
+    return total + item.count * item.price;
+  }, 0);
+
+  if (applyDiscount) {
+    return sum * 0.5;
+  }
+  return sum;
+}
